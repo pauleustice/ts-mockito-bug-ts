214aaf47865c061ed73da94752ce843c371c4ffa
Paul Eustice <1677737+pauleustice@users.noreply.github.com>
2022-07-19 16:56:12 +0100

    chore: strip out bumpf, rename app

--- apps/mockito-test/jest.config.ts
+++ apps/mockito-test/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'foo',
+  displayName: 'mockito-test',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/foo',
+  coverageDirectory: '../../coverage/apps/mockito-test',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
